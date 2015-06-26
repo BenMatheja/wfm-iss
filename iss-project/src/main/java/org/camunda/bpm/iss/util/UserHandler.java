c9ba7c98e55489f9d904479c79d54b94ecfb1e75
ShabdizG <s_ghas02@uni-muenster.de>
2015-06-26 14:11:03 +0200

    added process,task and identityServices

--- iss-project/src/main/java/org/camunda/bpm/iss/util/UserHandler.java
+++ iss-project/src/main/java/org/camunda/bpm/iss/util/UserHandler.java
@@ -3,16 +3,21 @@ package org.camunda.bpm.iss.util;
 import java.util.Arrays;
 import java.util.List;
 
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.identity.User;
-import org.camunda.bpm.engine.*;
-import org.camunda.bpm.*;
-
-private TaskService taskService;
-
 
 
 public class UserHandler {
 
+	private ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	private TaskService taskService = processEngine.getTaskService();
+	
+	private IdentityService is = processEngine.getIdentityService();
+	private List<User> users = is.createUserQuery().list();
+	
 	public List<String> findDifferentUser(String contractuser) {
 		List<String> userList = Arrays.asList("mary", "peter", "demo");
 		for(String user: userList) {
