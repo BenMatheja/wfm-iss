a1aff3b2fc560aa5f8c51ab229ef9d1e0b91deac
karstenrothmeier <karsten.rothmeier@uni-muenster.de>
2015-07-07 12:12:05 +0200

    inputGraphicsDesign not working yet

--- iss-project/src/main/java/org/camunda/bpm/iss/web/InputGraphicsDesignController.java
+++ iss-project/src/main/java/org/camunda/bpm/iss/web/InputGraphicsDesignController.java
@@ -1,5 +1,6 @@
 package org.camunda.bpm.iss.web;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,8 +12,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
+import org.camunda.bpm.iss.api.mock.SendThread;
 import org.camunda.bpm.iss.ejb.ProjectService;
 import org.camunda.bpm.iss.entity.Project;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
 
 @Named
 @ConversationScoped
@@ -34,6 +39,9 @@ public class InputGraphicsDesignController implements Serializable{
 	  @Inject
 	  private ProjectService projectService;
 	  
+	  @Inject
+	  private TaskForm taskForm;
+	  
 	  // Caches the Entities during the conversation
 	  private Project projectEntity; 
 
@@ -47,6 +55,33 @@ public class InputGraphicsDesignController implements Serializable{
 	      projectEntity = projectService.getProject((Long) businessProcess.getVariable("projectId"));
 	    }
 	    return projectEntity;
+	  }
+	  
+	  public void submit() throws IOException {	
+		  // Create JSON
+		  ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		  String json = ow.writeValueAsString(designRequest);
+		  // Create url
+		  String url = URL_API_PB_RECEIVE_DESIGN_REQUEST;
+		  
+		  //Send 
+		  SendThread sendThread = new SendThread(json,url,0,"Create Send Thread for designRequest");
+		  sendThread.run();
+		  
+		  
+		  /**
+		  designRequest = designRequestService.create(designRequest);
+		  businessProcess.setVariable("designRequestId", designRequest.getId());
+		  
+		  LOGGER.log(Level.INFO, "This is designRequestId in the business process: " + businessProcess.getVariable("designRequestId"));
+		  
+		  DesignRequest persistedDesignRequest = designRequestService.getDesignRequest(designRequest.getId());
+		  LOGGER.log(Level.INFO, "This is the persisted DesingRequest: ");
+		  LOGGER.log(Level.INFO, " Title: "+ persistedDesignRequest.getTitle());
+		  LOGGER.log(Level.INFO, " Description: "+ persistedDesignRequest.getDescription());
+			**/
+		  
+		  taskForm.completeTask();
 	  }
 
 }
