7dec5c93a52cb4c8c3212423038006ddc1520529
karstenrothmeier <karsten.rothmeier@uni-muenster.de>
2015-07-07 16:03:30 +0200

    Create Project Team Idea

---
+++ iss-project/src/main/java/org/camunda/bpm/iss/util/CreateProjectTeam.java
@@ -0,0 +1,30 @@
+package org.camunda.bpm.iss.util;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.iss.ejb.ContractService;
+
+
+
+public class CreateProjectTeam {
+	
+	private ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	private IdentityService is = processEngine.getIdentityService();
+	private static Logger LOGGER = Logger.getLogger(ContractService.class.getName());
+	
+	public void create(DelegateExecution delegateExecution) {
+		Map<String, Object> variables = delegateExecution.getVariables();
+		Long projectId = (Long) variables.get("projectId");
+		
+		LOGGER.log(Level.INFO, "Group name should be: project-team" + projectId); 
+		is.newGroup("project-team"+projectId);
+		
+		
+	}
+}
