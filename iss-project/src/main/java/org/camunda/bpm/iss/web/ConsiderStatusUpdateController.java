c5d9a922fd097f6f0a817c9737d5db0b08fb3d9a
ShabdizG <s_ghas02@uni-muenster.de>
2015-07-10 19:00:53 +0200

    added some things.. and stuff

--- iss-project/src/main/java/org/camunda/bpm/iss/web/ConsiderStatusUpdateController.java
+++ iss-project/src/main/java/org/camunda/bpm/iss/web/ConsiderStatusUpdateController.java
@@ -8,7 +8,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.iss.ejb.CustomerService;
+import org.camunda.bpm.iss.ejb.ProjectService;
 import org.camunda.bpm.iss.ejb.StatusUpdateService;
+import org.camunda.bpm.iss.entity.Customer;
 
 @Named
 @ConversationScoped
@@ -18,15 +21,39 @@ public class ConsiderStatusUpdateController implements Serializable{
 	StatusUpdateService statusUpdateService;
 	
 	@Inject 
+	CustomerService customerService;
+	
+	@Inject 
+	ProjectService projectService;
+	
+	@Inject 
 	BusinessProcess businessProcess;
+	
+	Customer customerEntity = getCustomer();
 	
 	long statusUpdateId = (Long) businessProcess.getVariable("statusUpdateId");
 	
+	public Customer getCustomer(){
+		return customerService.getCustomer((Long)businessProcess.getVariable("customerId"));
+	}
+	
+	//output Date of Status Update received
 	public Date getDate(){
 		return statusUpdateService.getDate(statusUpdateId);
 	}
 	
+	//output Status Update Message
 	public String getMessage(){
 		return statusUpdateService.getMessage(statusUpdateId);
+	}
+	
+	//output Project Title
+	public String getProjectTitle(){
+		return projectService.getProject((Long) businessProcess.getVariable("projectId")).getTitle();
+	}
+	
+	//output Design Title
+	public String getDesignRequestTitle(){
+		return businessProcess.getVariable("designRequestTitle");
 	}
 }
