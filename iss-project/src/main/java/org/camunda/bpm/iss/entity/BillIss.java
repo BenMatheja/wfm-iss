b3283aae89e91826276806cc55d253b906d8f6bf
ShabdizG <s_ghas02@uni-muenster.de>
2015-07-13 12:13:04 +0200

    BillIss.java entity

--- iss-project/src/main/java/org/camunda/bpm/iss/entity/BillIss.java
+++ iss-project/src/main/java/org/camunda/bpm/iss/entity/BillIss.java
@@ -1,6 +1,8 @@
 package org.camunda.bpm.iss.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,8 +21,32 @@ public class BillIss implements Serializable {
 	@Lob
 	private byte[] bill;
 	
-	String comment;
+	private String comment;
 			
+	private int priceInCent;
+	
+	private int costEstimation;
+	
+	private String customerName;
+	
+	private String projectTitle;
+	
+	private String contractTitle;
+	 
+	private Collection<Employee> employees;
+	
+	private Date projectStart;
+	
+	private Date projectEnd;
+	
+	private long accumulatedHours;
+	
+	private double pbTotal;
+	
+	private int issTotal;
+	
+		
+	
 	public long getId() {
 		return id;
 	}	
@@ -40,5 +66,96 @@ public class BillIss implements Serializable {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
+	public int getPriceInCent() {
+		return priceInCent;
+	}
+
+	public void setPriceInCent(int priceInCent) {
+		this.priceInCent = priceInCent;
+	}
+
+	public int getCostEstimation() {
+		return costEstimation;
+	}
+
+	public void setCostEstimation(int costEstimation) {
+		this.costEstimation = costEstimation;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getProjectTitle() {
+		return projectTitle;
+	}
+
+	public void setProjectTitle(String projectTitle) {
+		this.projectTitle = projectTitle;
+	}
+
+	public String getContractTitle() {
+		return contractTitle;
+	}
+
+	public void setContractTitle(String contractTitle) {
+		this.contractTitle = contractTitle;
+	}
+
+	public Collection<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(Collection<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public Date getProjectStart() {
+		return projectStart;
+	}
+
+	public void setProjectStart(Date projectStart) {
+		this.projectStart = projectStart;
+	}
+
+	public Date getProjectEnd() {
+		return projectEnd;
+	}
+
+	public void setProjectEnd(Date projectEnd) {
+		this.projectEnd = projectEnd;
+	}
+
+	public long getAccumulatedHours() {
+		return accumulatedHours;
+	}
+
+	public void setAccumulatedHours(long accumulatedHours) {
+		this.accumulatedHours = accumulatedHours;
+	}
+
+	public double getPbTotal() {
+		return pbTotal;
+	}
+
+	public void setPbTotal(double pbTotal) {
+		this.pbTotal = pbTotal;
+	}
+
+	public int getIssTotal() {
+		return issTotal;
+	}
+
+	public void setIssTotal(int issTotal) {
+		this.issTotal = issTotal;
+	}
+	
+	
+	
 		
 }
